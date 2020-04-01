b46c46ad508b2996b064ae4efeca5778dc91d3fa
gevorgepamworks <gevorgitmkrtchyan@gmail.com>
2020-04-01 20:46:58 +0400

    basic structure

--- JPA-Demo/src/main/java/com/example/JPA/Demo/Author.java
+++ JPA-Demo/src/main/java/com/example/JPA/Demo/Author.java
@@ -1,4 +1,57 @@
 package com.example.JPA.Demo;
 
+import java.util.Set;
+
 public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
